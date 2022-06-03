f94b071797ebe87c333007a771b35f3e2affaedf
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-06-03 20:52:43 -0300

    moving screen attributes to context struct

--- src/nativeInterop/cinterop/kitty_extra.h
+++ src/nativeInterop/cinterop/kitty_extra.h
@@ -8,11 +8,62 @@
 #include <termios.h>
 #include <stdbool.h>
 #include <string.h>
+#include <signal.h>
 
 #define ESC "\x1B" /* ESCAPE */
 #define BKS "\x5c" /* BACKSLASH */
 
 char error_msg[256] = "\0";
+
+
+static void kitty_println(const char* msg);
+static bool kitty_get_window_size(struct winsize* sz);
+
+
+void _signal_handler(int signum) {
+    char *msg[50];
+    snprintf(msg, "Interrupt signal (%u) received.", signum);
+    kitty_println(msg);
+
+    // cleanup and close up stuff here
+    // terminate program
+
+    exit(signum);
+}
+
+
+typedef struct /* using Kotlin naming convention */
+{
+	unsigned int rows;
+	unsigned int cols;
+	unsigned int width;
+	unsigned int height;
+	unsigned int cellWidth;
+	unsigned int cellHeight;
+} KittyContext;
+
+
+static bool kitty_create_context(KittyContext* ctx)
+{
+    kitty_setup_termios();
+
+    // register signal SIGINT and signal handler
+    signal(SIGWINCH, _signal_handler);
+
+    struct winsize sz;
+    if (kitty_get_window_size(&sz)) {
+        ctx->rows = sz.ws_row;
+        ctx->cols = sz.ws_col;
+        ctx->width = sz.ws_xpixel;
+        ctx->height = sz.ws_ypixel;
+        ctx->cellWidth = sz.ws_xpixel / sz.ws_col;
+        ctx->cellHeight = sz.ws_ypixel / sz.ws_row;
+        return true;
+    }
+
+    return false;
+}
+
 
 static void print_error_msg()
 {
@@ -24,7 +75,7 @@ static void print_error_msg()
 }
 
 
-static void die(const char* msg)
+static void kitty_die(const char* msg)
 {
     kitty_restore_termios();
     printf("* %s\n", msg);
@@ -32,7 +83,7 @@ static void die(const char* msg)
 }
 
 
-static void println2(const char* msg)
+static void kitty_println(const char* msg)
 {
     kitty_restore_termios();
     printf("%s\n", msg);
@@ -40,7 +91,7 @@ static void println2(const char* msg)
 }
 
 
-static bool get_window_size(struct winsize* sz)
+static bool kitty_get_window_size(struct winsize* sz)
 {
     ioctl(0, TIOCGWINSZ, sz);
     if (sz->ws_xpixel && sz->ws_ypixel) {
@@ -63,7 +114,7 @@ static bool get_random_number()
 }
 
 
-static bool check_graphics_support()
+static bool kitty_check_graphics_support()
 {
     // sending RGB(f=24) image with ID(i)=1 and width(s)=1 and height(v)=1
     static char* graphics_query = ESC "_Gi=31,s=1,v=1,a=q,t=d,f=24;AAAA" ESC BKS ESC "[c";
@@ -75,7 +126,7 @@ static bool check_graphics_support()
 }
 
 
-static bool check_local_execution()
+static bool kitty_check_local_execution()
 {
     char buf[PATH_MAX];
     const size_t BASE64_SIZE = ((PATH_MAX + 2) / 3) * 4;
@@ -108,14 +159,14 @@ static bool check_local_execution()
             }
         }
 
-        die(error_msg);
+        kitty_die(error_msg);
     }
 
     return false;
 }
 
 
-static bool store_image(unsigned int id, const char* fname)
+static bool kitty_store_image(unsigned int id, const char* fname)
 {
     char buf[PATH_MAX];
     const size_t BASE64_SIZE = ((PATH_MAX + 2) / 3) * 4;
@@ -148,7 +199,7 @@ static bool store_image(unsigned int id, const char* fname)
             }
         }
 
-        die(error_msg);
+        kitty_die(error_msg);
     }
 
     return false;
@@ -216,7 +267,7 @@ static bool _image_cmd(const char* command, struct Image* data)
             }
         }
 
-        die(error_msg);
+        kitty_die(error_msg);
     }
 
     return false;
@@ -237,7 +288,7 @@ static bool free_image(unsigned int id)
 }
 
 
-static bool display_image(unsigned int id)
+static bool kitty_display_image(unsigned int id)
 {
     struct Image data = { id, 0, 0, false, {}, false, {} };
 
@@ -248,7 +299,7 @@ static bool display_image(unsigned int id)
 }
 
 
-static bool display_image2(unsigned int id, unsigned int placement)
+static bool kitty_display_image2(unsigned int id, unsigned int placement)
 {
     struct Image data = { id, placement, 0, false, {}, false, {} };
 
@@ -259,7 +310,7 @@ static bool display_image2(unsigned int id, unsigned int placement)
 }
 
 
-static bool display_image3(unsigned int id, unsigned int placement, unsigned int zindex)
+static bool kitty_display_image3(unsigned int id, unsigned int placement, unsigned int zindex)
 {
     struct Image data = { id, placement, zindex, false, {}, false, {} };
 
@@ -270,7 +321,7 @@ static bool display_image3(unsigned int id, unsigned int placement, unsigned int
 }
 
 
-static bool hide_image(unsigned int id, unsigned int placement)
+static bool kitty_hide_image(unsigned int id, unsigned int placement)
 {
     // prepare command
     char command[20];
--- src/nativeMain/kotlin/hello.kt
+++ src/nativeMain/kotlin/hello.kt
@@ -1,67 +1,52 @@
 import kitty_util.*
 
 import kotlinx.cinterop.alloc
-//import kotlinx.cinterop.cValue
+import kotlinx.cinterop.cValue
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.ptr
-//import kotlinx.cinterop.readValue
+
 
 fun main() {
-    var width : UShort?
-    var height : UShort?
-    var rows: UShort?
-    var cols: UShort?
-    var cell_w: UInt?
-    var cell_h: UInt?
     var local_exec : Boolean?
 
     memScoped {
-        kitty_setup_termios()
-
-        val size = alloc<winsize>()
-
-        if (get_window_size(size.ptr)) {
-            width = size.ws_xpixel
-            height = size.ws_ypixel
-            cols = size.ws_col
-            rows = size.ws_row
-            cell_w = size.ws_xpixel / size.ws_col
-            cell_h = size.ws_ypixel / size.ws_row
-        } else {
-            die("Kitty required: window size is unknown.")
-            return
+        val ctx = alloc<KittyContext> {
+            rows = 0u; cols = 0u; width = 0u; height = 0u; cellWidth = 0u; cellHeight = 0u
         }
 
-        if (!check_graphics_support()) {
-            die("Kitty required: graphics support not supported.")
-            return
+	if (!kitty_create_context(ctx.ptr)) {
+            kitty_die("Kitty required: window size is unknown.")
+	} 
+
+        if (!kitty_check_graphics_support()) {
+            kitty_die("Kitty required: graphics support not supported.")
         }
 
-        local_exec = check_local_execution().let {
+        local_exec = kitty_check_local_execution().let {
             if (it) {
-                println2("* Local execution detected.")
+                kitty_println("* Local execution detected.")
             } else {
-                println2("* Non-local execution detected.")
+                kitty_println("* Non-local execution detected.")
             }
             it;
         }
 
-        store_image(1, "./assets/cow_large.png").let {
+        kitty_store_image(1, "./assets/cow_large.png").let {
             if (it) {
-                display_image(1).let {
-                    if (it) println2("\n* Image displayed.")
+                kitty_display_image(1).let {
+                    if (it) kitty_println("\n* Image displayed.")
                 }
             } else {
-                println2("* Image couldn't be stored.")
+                kitty_println("* Image couldn't be stored.")
             }
         }
 
-        kitty_restore_termios();
-        println("* Window size is $width x $height.")
-        println("* Window size is $cols x $rows cells.")
-        println("* Cell size is $cell_w x $cell_h.")
+        kitty_restore_termios()
+        println("* Window size is ${ctx.width} x ${ctx.height}.")
+        println("* Window size is ${ctx.cols} x ${ctx.rows} cells.")
+        println("* Cell size is ${ctx.cellWidth} x ${ctx.cellHeight}.")
         println("* Graphics support is OK.")
-
-        return;
     }
+
+    return;
 }
